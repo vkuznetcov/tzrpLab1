58d18445bd4626ae76383d14cc4e1771b0eb07c4
slav <vkuznetcov48@gmail.com>
2022-09-27 01:03:11 +0400

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,59 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.Socket;
+import java.util.Random;
+
+public class Client {
+    public static void main(String[] args) {
+
+        try (Socket socket = new Socket("127.0.0.1", 8080);
+             OutputStream outputStream = socket.getOutputStream()) {
+            BufferedImage image = ImageIO.read(new File("resources/lilnicex.jpg"));
+            BufferedImage noisedImage = addNoise(image);
+            ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+            ImageIO.write(noisedImage, "jpeg", byteBuffer);
+            outputStream.write(byteBuffer.toByteArray());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private static BufferedImage addNoise(BufferedImage originalImage) {
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight();
+
+        for (int i = 0; i < height; i++) {
+
+            for (int j = 0; j < width; j++) {
+
+                Color c = new Color(originalImage.getRGB(j, i));
+                int red = (int) (c.getRed() * 0.299);
+                int green = (int) (c.getGreen() * 0.587);
+                int blue = (int) (c.getBlue() * 0.114);
+                int rgb = range(red + green + blue, 8);
+                Color newColor = new Color(rgb, rgb, rgb);
+
+                originalImage.setRGB(j, i, newColor.getRGB());
+            }
+        }
+        return originalImage;
+    }
+
+    private static int range(int n, double prob) {
+        double res = ((100 * prob) / 10);
+
+        int[] array = new int[(int) res];
+        array[0] = 1;
+        array[1] = 255;
+
+        for (int i = 2; i <= res - 2; i++) {
+            array[i] = n;
+        }
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
+    }
+
+}
---
+++ src/Server.java
@@ -0,0 +1,67 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("server started");
+        try(ServerSocket serverSocket = new ServerSocket(8080);
+            Socket clientSocket = serverSocket.accept();
+            InputStream inputStream = clientSocket.getInputStream()){
+            byte[] imageBytes = inputStream.readAllBytes();
+            BufferedImage noisedImage = readFromBytes(imageBytes);
+            BufferedImage filteredImage = filterImage(noisedImage);
+            ImageIO.write(noisedImage, "jpg", new File(
+                    "C:\\Users\\vkuzn\\Desktop\\tzrpLab1\\lilnicenoised.jpg"));
+            ImageIO.write(filteredImage, "jpg", new File(
+                    "C:\\Users\\vkuzn\\Desktop\\tzrpLab1\\lilnicefiltered.jpg"));
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static BufferedImage readFromBytes(byte[] imageBytes) throws IOException {
+        ByteArrayInputStream byteArrayBuffer= new ByteArrayInputStream(imageBytes);
+        return ImageIO.read(byteArrayBuffer);
+    }
+
+    private static BufferedImage filterImage(BufferedImage noisedImage){
+        Color[] pixel=new Color[9];
+        int[] R = new int[9];
+        int[] B = new int[9];
+        int[] G = new int[9];
+
+        for(int i=1;i<noisedImage.getWidth()-1;i++)
+            for(int j=1;j<noisedImage.getHeight()-1;j++)
+            {
+                pixel[0]=new Color(noisedImage.getRGB(i-1,j-1));
+                pixel[1]=new Color(noisedImage.getRGB(i-1,j));
+                pixel[2]=new Color(noisedImage.getRGB(i-1,j+1));
+                pixel[3]=new Color(noisedImage.getRGB(i,j+1));
+                pixel[4]=new Color(noisedImage.getRGB(i+1,j+1));
+                pixel[5]=new Color(noisedImage.getRGB(i+1,j));
+                pixel[6]=new Color(noisedImage.getRGB(i+1,j-1));
+                pixel[7]=new Color(noisedImage.getRGB(i,j-1));
+                pixel[8]=new Color(noisedImage.getRGB(i,j));
+                for(int k=0;k<9;k++){
+                    R[k]=pixel[k].getRed();
+                    B[k]=pixel[k].getBlue();
+                    G[k]=pixel[k].getGreen();
+                }
+                Arrays.sort(R);
+                Arrays.sort(G);
+                Arrays.sort(B);
+                noisedImage.setRGB(i,j,new Color(R[4],B[4],G[4]).getRGB());
+            }
+        return noisedImage;
+    }
+}
